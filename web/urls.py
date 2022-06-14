aa14c39c0cbd575da1ad8c1ae5f7ff38bbf06a2f
Kollaider <kolja87@tut.by>
2022-06-14 19:47:12 +0300

    added templates

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from web.views import index, detail, about
+
+app_name = "web"
 urlpatterns = [
-    path('', index),
-    path('detail/<int:pk>', detail)
+    path('', index, name="home"),
+    path('detail/<int:pk>', detail, name="detail"),
+    path('about/', about, name='about')
 ]
--- web/views.py
+++ web/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html')
+from web.models import Card
 
-def detail(request):
-    return render(request, 'detail.html')
+
+def index(request):
+    cards = Card.objects.all()
+
+    return render(request, 'index.html', {'cards': cards})
+
+
+def detail(request, pk):
+    card = Card.objects.get(id=pk)
+
+    context = {'card': card}
+    return render(request, 'detail.html', context=context)
+
+def about(request):
+    return render(request, 'about.html')
