a0416e1bb34d17acd0d847677a1197a057e155a8
Kollaider <kolja87@tut.by>
2022-06-19 16:13:47 +0300

    new app blog

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import blog_index, blog_detail, blog_category
+
+app_name = "blog"
+urlpatterns = [
+    path('', blog_index, name='blog_index'),
+    path("<int:pk>/", blog_detail, name='blog_detail'),
+    path("<category>/", blog_category, name='blog_category')
+]
